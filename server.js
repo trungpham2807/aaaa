5291c2d64dfb393acb72e50290f0e3c62ce8d275
leonero2807 <trungpham280797@gmail.com>
2021-11-27 03:02:36 +0700

    a

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-import dotenv from 'dotenv';
+require('dotenv').config()
 
-dotenv.config();
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const PASSWORD = process.env.MONGO_PASSWORD;
 app.use(bodyParser.urlencoded({ extended: true })); 
